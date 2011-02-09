34cb05b9f38dc4e82784d8dd3853a9dcae6bd83a
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2011-02-09 23:25:42 +0000

    faster algorithm (heavily SQL based) implemented, but not completed yet
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@98 f7c66474-1b4d-40db-9de1-ad65934439f2

--- public_html/cgi-bin/sum_cat_disc.py
+++ public_html/cgi-bin/sum_cat_disc.py
@@ -110,22 +110,22 @@ displaystate_content = \
 _SQL_query_ns_content = \
 """SELECT page_title, page_namespace
 FROM page
-JOIN categorylinks ON cl_from = page_id WHERE cl_to = "%s"
-LIMIT %i"""
+JOIN categorylinks ON cl_from = page_id WHERE cl_to = %s
+LIMIT %s"""
 
 #_SQL_query_page_info = \
 #"""SELECT page_title, page_namespace, page_touched
 #FROM page
 #WHERE page_title LIKE "%s"
 #AND page_namespace IN %s
-#LIMIT %i"""
+#LIMIT %s"""
 
 _SQL_query_page_info = \
 """SELECT page_title, page_namespace, page_touched
 FROM page
-WHERE page_title LIKE "%s"
-AND page_namespace = %i
-LIMIT %i"""
+WHERE page_title LIKE %s
+AND page_namespace = %s
+LIMIT %s"""
 
 #_SQL_query__ = \
 #"""SELECT page_title, page_namespace, page_touched
@@ -135,31 +135,53 @@ LIMIT %i"""
 #FROM page
 #JOIN categorylinks ON cl_from = page_id WHERE cl_to = "%s")
 #AND page_namespace IN (1, 3, 5, 9, 11, 13, 15, 91, 93, 101, 109)
-#LIMIT %i"""
+#LIMIT %s"""
 
 # written by Merlissimo (merl) - Thanks a lot!
 # http://kpaste.net/da2d, http://kpaste.net/5b2f97f
 # ~/devel/experimental/diskchanges.sql
-# (need db write access; e.g. not possible during 'schema changes in progress')
-_SQL_query_01 = \
-"""CREATE TEMPORARY TABLE cattemp (catname VARCHAR(255) PRIMARY KEY);
-CREATE TEMPORARY TABLE cat (catname VARCHAR(255) PRIMARY KEY, depth INT, INDEX(depth));
-INSERT IGNORE INTO cat (catname, depth) SELECT page_title, 0 FROM dewiki_p.page WHERE page_namespace=14 AND page_title='%s';"""
-_SQL_query_02 = \
-"""DELETE FROM cattemp;
-INSERT IGNORE INTO cattemp (catname) SELECT catname FROM cat WHERE depth=%i;
-INSERT IGNORE INTO cat (catname, depth) SELECT page_title, %i FROM cattemp INNER JOIN dewiki_p.categorylinks ON catname=cl_to INNER JOIN dewiki_p.page ON cl_from = page_id WHERE page_namespace=14;"""
-_SQL_query_03 = \
+# need db write access; to create temporary tables (they live until the db connection
+# gets closed) and is thus not possible during e.g. 'schema changes in progress'
+# -----------------------------------------------------------------------------
+# drtrigon@nightshade:~$ mysql -hdewiki-p.userdb u_drtrigon
+# ERROR 1049 (42000): Unknown database 'u_drtrigon'
+# drtrigon@nightshade:~$ mysql -hdewiki-p.userdb
+# Welcome to the MySQL monitor.  Commands end with ; or \g.
+# Your MySQL connection id is 22197636
+# Server version: 5.1.53 Source distribution
+#
+# Copyright (c) 2000, 2010, Oracle and/or its affiliates. All rights reserved.
+# This software comes with ABSOLUTELY NO WARRANTY. This is free software,
+# and you are welcome to modify and redistribute it under the GPL v2 license
+#
+# Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+#
+# mysql> CREATE DATABASE IF NOT EXISTS u_drtrigon
+#     -> ;
+# Query OK, 1 row affected (0.02 sec)
+# -----------------------------------------------------------------------------
+_SQL_query_cattree = [
+"""CREATE TEMPORARY TABLE cattemp (catname VARCHAR(255) PRIMARY KEY);""",
+"""CREATE TEMPORARY TABLE cat (catname VARCHAR(255) PRIMARY KEY, depth INT, INDEX(depth));""",
+"""INSERT IGNORE INTO cat (catname, depth) SELECT page_title, 0 FROM %swiki_p.page WHERE page_namespace=14 AND page_title=%s;""",
+
+"""DELETE FROM cattemp""",
+"""INSERT IGNORE INTO cattemp (catname) SELECT catname FROM cat WHERE depth=%s""",
+"""INSERT IGNORE INTO cat (catname, depth) SELECT page_title, %s FROM cattemp INNER JOIN %swiki_p.categorylinks ON catname=cl_to INNER JOIN %swiki_p.page ON cl_from = page_id WHERE page_namespace=14""",
+"""SELECT ROW_COUNT()""",
+
 """SELECT rc_namespace, rc_title, rc_user_text, rc_timestamp, rc_comment
  FROM cat
-  INNER JOIN dewiki_p.categorylinks ON ct = cl_to
+  INNER JOIN dewiki_p.categorylinks ON catname = cl_to
   INNER JOIN dewiki_p.page p1 ON cl_from = page_id
   INNER JOIN dewiki_p.page p2 ON p1.page_title = p2.page_title AND p1.page_namespace+1=p2.page_namespace
   INNER JOIN dewiki_p.recentchanges ON p2.page_id = rc_cur_id
  WHERE p1.page_namespace IN (0,10,14)
   AND rc_timestamp >= DATE_SUB(CURDATE(),INTERVAL 3 DAY)
   AND rc_old_len < rc_new_len
- ORDER BY rc_timestamp DESC;"""
+ ORDER BY rc_timestamp DESC;""",
+
+"""SELECT VERSION();""", ]
 
 # https://wiki.toolserver.org/view/Database_access#wiki
 _SQL_query_wiki_info = \
@@ -170,7 +192,7 @@ _SQL_query_wiki_info = \
    CONCAT("http://", DOMAIN, script_path, "api.php") AS url
  FROM toolserver.wiki
  WHERE family = "wikipedia"
- ORDER BY SIZE DESC LIMIT %i;"""
+ ORDER BY SIZE DESC LIMIT %s;"""
 
 
 SQL_LIMIT_max = 1000
@@ -178,16 +200,23 @@ SQL_LIMIT_max = 1000
 wikitime = "%Y%m%d%H%M%S"
 
 
-def query_db(query, limit=SQL_LIMIT_max):
-	db.query(query)
-	r = db.store_result()
+def call_db(query, args=(), limit=SQL_LIMIT_max):
+	#db.query(query % args)
 
-	return [row for row in r.fetch_row(limit)]
+	# execute SQL query using execute() method.
+	# for strings " or ' has to be omitted, execute inserts them as well
+	cursor.execute(query, args)
+
+def read_db(query, args=(), limit=SQL_LIMIT_max):
+	call_db(query, args, limit=limit)
+
+	#return db.store_result().fetch_row(limit)
+	return cursor.fetchmany(limit)
 
 # recursive
 def get_cat_tree_rec(cat, limit=SQL_LIMIT_max):
 	res = set([])
-	for row in query_db(_SQL_query_ns_content % (cat.replace(' ', '_'), limit)):
+	for row in read_db(_SQL_query_ns_content, (cat.replace(' ', '_'), limit)):
 		# Category
 		if int(row[1]) == 14:
 			res = res.union( get_cat_tree_rec(row[0], limit) )
@@ -199,28 +228,49 @@ def get_cat_tree_rec(cat, limit=SQL_LIMIT_max):
 	return res
 
 # iterative (heavily SQL based)
-def get_cat_tree_it(cat, limit=SQL_LIMIT_max):
-        db.query(_SQL_query_01 % cat.replace(' ', '_'))
-        r = db.store_result()
-	return r
+def checkRecentEdits_db_it(cat, end, limit=SQL_LIMIT_max):
+	res = []
 
+#	db.query(("\n".join(_SQL_query_01)) % cat.replace(' ', '_'))
+
+	c=cursor
+	call_db(_SQL_query_cattree[0])
+	call_db(_SQL_query_cattree[1])
+	#call_db(_SQL_query_cattree[2], (cat.replace(' ', '_'),))
+	_SQL_query = _SQL_query_cattree[2] % ('de', '%s')  # BAD: SQL injection!
+	call_db(_SQL_query, (cat.replace(' ', '_'),))      #
+
+#	uu = read_db(_SQL_query_cattree[8], limit=limit)
+#	res.append( uu )
+#	return res
+
+	_SQL_query = _SQL_query_cattree[5] % ('%s', 'de', 'de')  # BAD: SQL injection!
 	for i in range(25):
-	        db.query(_SQL_query_02 % (i, i+1))
-        	r = db.store_result()
+		call_db(_SQL_query_cattree[3])
+		call_db(_SQL_query_cattree[4], (i,))
+		#call_db(_SQL_query_cattree[5], (i+1,))
+		call_db(_SQL_query, (i+1,))                      # BAD: SQL injection!
+		co = read_db(_SQL_query_cattree[6], limit=limit)
 
-#	int [] co = stmt.executeBatch();
-#	if(co[2] == 0) break;
+		if (co[0][0] == 0): break
 
-        db.query(_SQL_query_03)
-        r = db.store_result()
+	unique = set([])
+	for page in list(read_db(_SQL_query_cattree[7], limit=limit)):
+# there is a parameter 'INTERVAL 3 DAY' in '_SQL_query_cattree' is this a problem??
+		u = page[:2] # make list items unique, BUT YOU HAVE TO TAKE THE NEWEST ONE!!!
+		if (int(page[3]) >= end) and (u not in unique):
+			res.append( [(page[1], page[0], page[3])] )
+		unique.add(u)
 
-def checkRecentEdits_db(cat, end, limit=SQL_LIMIT_max):
+	return res
+
+def checkRecentEdits_db_rec(cat, end, limit=SQL_LIMIT_max):
 	res = []
 	for row in list(get_cat_tree_rec(cat, limit)):
 		ns = row[1]
 		ns = ns + (1 - (ns % 2))  # next bigger odd
 		try:
-			subrow = query_db(_SQL_query_page_info % (row[0], ns, 1))
+			subrow = read_db(_SQL_query_page_info, (row[0], ns, 1))
 			if subrow and (int(subrow[0][2]) >= end):
 				res.append( subrow )
 		except _mysql_exceptions.ProgrammingError:  # are displayed later with '(!)' mark
@@ -250,7 +300,7 @@ def checkRecentEdits_API(cat, end):
 	return res
 
 def get_wikiinfo_db(wiki, limit=SQL_LIMIT_max):
-	for item in query_db(_SQL_query_wiki_info % limit, limit):
+	for item in read_db(_SQL_query_wiki_info, (limit,), limit):
 		if item[2] == (wiki + "wiki_p"):
 			return item
 	return None
@@ -261,6 +311,8 @@ def displayhtmlpage(form):
 	start  = form.getvalue('start', datetime.datetime.utcnow().strftime(wikitime))
 #	start  = form.getvalue('start', dtbext.date.getTimeStmp(full=True))
 	period = form.getvalue('period', '24')
+
+#	period = '10000'
 
 	s = datetime.datetime.strptime(start, wikitime)
 	p = datetime.timedelta(hours=int(period))
@@ -277,19 +329,20 @@ def displayhtmlpage(form):
 	data['output'] += "<br>\n"
 
 #	talk_ns = (1, 3, 5, 9, 11, 13, 15, 91, 93, 101, 109)
-#	res += str( query_db(_SQL_query_page_info % ('%DrTrigon',str(talk_ns),10 )) )
+#	res += str( read_db(_SQL_query_page_info, ('%DrTrigon',str(talk_ns),10)) )
 
-#	res += str( query_db(_SQL_query_02 % (cat, )) )
+#	res += str( read_db(_SQL_query_02, (cat,)) )
 #	res += "<br>\n"
 
 #	cat = 'Portal:Hund'
-#	data['output'] = str( get_cat_tree_it(cat, limit=SQL_LIMIT_max) )
+#	cat = 'Baden-Württemberg'
 #	cat = ''
 
 	if cat:
 		tic = time()
-#               out = checkRecentEdits_API(cat, end)
-                out = checkRecentEdits_db(cat, end)
+#		out = checkRecentEdits_API(cat, end)
+		out = checkRecentEdits_db_rec(cat, end)
+#		out = checkRecentEdits_db_it(cat, end)
 		toc = time()
 		if out:
 			# sort items/entries by timestamp
@@ -349,8 +402,12 @@ wiki = form.getvalue('wiki', 'de')
 # Establich a connection
 #db = MySQLdb.connect(db='enwiki_p', host="enwiki-p.rrdb.toolserver.org", read_default_file="/home/drtrigon/.my.cnf")
 db = MySQLdb.connect(db=wiki+'wiki_p', host=wiki+"wiki-p.rrdb.toolserver.org", read_default_file="/home/drtrigon/.my.cnf")
+#db = MySQLdb.connect(db='u_drtrigon', host="dewiki-p.userdb.toolserver.org", read_default_file="/home/drtrigon/.my.cnf")
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
 
 # operational mode
 print displayhtmlpage(form)
 
+cursor.close()
 db.close()
