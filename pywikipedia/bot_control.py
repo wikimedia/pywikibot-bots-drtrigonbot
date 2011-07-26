5eec5dbea93f9e4ba048785ec1aaeaa6175d4286
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2011-07-26 08:26:33 +0000

    investigating bug DRTRIGON-87 with help of valhallasw@arctus.nl
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@131 f7c66474-1b4d-40db-9de1-ad65934439f2

--- pywikipedia/bot_control.py
+++ pywikipedia/bot_control.py
@@ -183,16 +183,16 @@ class BotErrorHandler:
 
 	def send_mailnotification(self, item):
 		pywikibot.output(u'Sending mail "%s" to "%s" as notification!' % (error_mail[1], error_mail[0]))
-		usr = userlib.User(pywikibot.getSite(), error_mail[0])
-		# JIRA: DRTRIGON-87; re-try loop, output error info
-		count = 3
-		while count:
-			count -= 1
-			try:
-				if usr.sendMail(subject=error_mail[1], text=item):	# 'item' should be unicode!
-					return
-			except:  # break exception handling recursion
-				pywikibot.output(u'!!! %s' % str(sys.exc_info()))
+		# JIRA: DRTRIGON-87; output even more debug info (tip by: valhallasw@arctus.nl)
+		site = pywikibot.getSite()
+		pywikibot.output(u'!!! Allowed to send email: %r' % (site.isAllowed('sendemail'),)
+		pywikibot.output(u'!!! Permissions: %r' % (site._rights,)
+		usr = userlib.User(site, error_mail[0])
+		try:
+			if usr.sendMail(subject=error_mail[1], text=item):	# 'item' should be unicode!
+				return
+		except:  # break exception handling recursion
+			pywikibot.output(u'!!! %s' % str(sys.exc_info()))
 		pywikibot.output(u'!!! WARNING: mail could not be sent!')
 
 	def gettraceback(self, exc_info):
