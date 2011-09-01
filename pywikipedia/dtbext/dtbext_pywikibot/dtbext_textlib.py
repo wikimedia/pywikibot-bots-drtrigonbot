33e33edb7d09aa8a0f617a9f0466394a5aa3217e
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2011-09-01 08:22:53 +0000

    PEP 8
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@164 f7c66474-1b4d-40db-9de1-ad65934439f2

--- pywikipedia/dtbext/dtbext_pywikibot/dtbext_textlib.py
+++ pywikipedia/dtbext/dtbext_pywikibot/dtbext_textlib.py
@@ -30,37 +30,37 @@ from HTMLParser import HTMLParser
 ## @since   r19 (ADDED)
 #  @remarks needed by various bots
 def removeHTMLParts(text, keeptags = ['tt', 'nowiki', 'small', 'sup']):
-	"""
-	Return text without portions where HTML markup is disabled
+    """
+    Return text without portions where HTML markup is disabled
 
-	Parts that can/will be removed are --
-	* HTML and all wiki tags
+    Parts that can/will be removed are --
+    * HTML and all wiki tags
 
-	The exact set of parts which should NOT be removed can be passed as the
-	'keeptags' parameter, which defaults to ['tt', 'nowiki', 'small', 'sup'].
-	"""
-	# try to replace with 'pywikibot.removeDisabledParts()' from 'textlib' !!
+    The exact set of parts which should NOT be removed can be passed as the
+    'keeptags' parameter, which defaults to ['tt', 'nowiki', 'small', 'sup'].
+    """
+    # try to replace with 'pywikibot.removeDisabledParts()' from 'textlib' !!
 
-	# thanks to http://www.hellboundhackers.org/articles/841-using-python-39;s-htmlparser-class.html
-	parser = _GetDataHTML()
-	parser.keeptags = keeptags
-	parser.feed(text)
-	parser.close()
-	return parser.textdata
+    # thanks to http://www.hellboundhackers.org/articles/841-using-python-39;s-htmlparser-class.html
+    parser = _GetDataHTML()
+    parser.keeptags = keeptags
+    parser.feed(text)
+    parser.close()
+    return parser.textdata
 
 # thanks to http://docs.python.org/library/htmlparser.html
 class _GetDataHTML(HTMLParser):
-	textdata = u''
-	keeptags = []
+    textdata = u''
+    keeptags = []
 
-	def handle_data(self, data):
-		self.textdata += data
+    def handle_data(self, data):
+        self.textdata += data
 
-	def handle_starttag(self, tag, attrs):
-		if tag in self.keeptags: self.textdata += u"<%s>" % tag
+    def handle_starttag(self, tag, attrs):
+        if tag in self.keeptags: self.textdata += u"<%s>" % tag
 
-	def handle_endtag(self, tag):
-		if tag in self.keeptags: self.textdata += u"</%s>" % tag
+    def handle_endtag(self, tag):
+        if tag in self.keeptags: self.textdata += u"</%s>" % tag
 
 #----------------------------------
 # Functions dealing with templates
@@ -69,18 +69,18 @@ class _GetDataHTML(HTMLParser):
 ## @since   r20 (ADDED)
 #  @remarks needed by various bots
 def glue_template_and_params(template_and_params):
-	"""Return wiki text of template glued from params.
+    """Return wiki text of template glued from params.
 
-	You can use items from extract_templates_and_params here to get
-	an equivalent template wiki text (it may happen that the order
-	of the params changes).
-	"""
-	(template, params) = template_and_params
+    You can use items from extract_templates_and_params here to get
+    an equivalent template wiki text (it may happen that the order
+    of the params changes).
+    """
+    (template, params) = template_and_params
 
-	text = u''
-	for item in params:
-		text +=  u'|%s=%s\n' % (item, params[item])
+    text = u''
+    for item in params:
+        text +=  u'|%s=%s\n' % (item, params[item])
 
-	return u'{{%s\n%s}}' % (template, text)
+    return u'{{%s\n%s}}' % (template, text)
 
 
