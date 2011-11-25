4a557f19f46ea98dc14a50081550034a9b058f19
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2011-11-25 18:13:11 +0000

    new feature; now the mail content can be viewed
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@215 f7c66474-1b4d-40db-9de1-ad65934439f2

--- public_html/cgi-bin/subster_mail_queue.py
+++ public_html/cgi-bin/subster_mail_queue.py
@@ -63,6 +63,7 @@ a = """<tr>
 <td>%s</td>
 <td>%s</td>
 </tr>"""
+b = "<a href='/~drtrigon/cgi-bin/subster_mail_queue.py?action=content&amp;no=%s'>%s</a>"
 
 
 # === functions === === ===
@@ -93,8 +94,36 @@ def displaystate(form):
 
             if sender:
                 # data found
-                buf.append( a % (i, cgi.escape(sender), subject, timestmp) )
+                buf.append( a % (i, (b % (i, cgi.escape(sender))), subject, timestmp) )
         buf.append( '</table>\n\n' )
+
+	data.update({   'oldlog':	"\n".join(buf),
+			'refresh':	'',
+			'title':	'DrTrigonBot subster mail queue (BETA)',
+			'tsnotice': 	style.print_tsnotice(),
+			'p-status':	"<tr><td><td></tr>",
+			'footer': 	style.footer + style.footer_w3c, # wiki (new) not CSS 2.1 compilant
+	})
+	data['content'] = displaystate_content % data
+
+	return style.page % data
+
+def content(form):
+	data = {}
+
+        no = int(form.getvalue('no', '-1'))
+        if no < 0: return displaystate(form)
+
+        mbox = mailbox.mbox('../../data/subster/mail_inbox')
+
+        buf = []
+
+        for i, message in enumerate(mbox):
+            if (i != no): continue
+            text = cgi.escape(message.as_string())
+            text = text.replace("\n", "<br>\n")
+            buf.append( text )
+        buf.append( '\n' )
 
 	data.update({   'oldlog':	"\n".join(buf),
 			'refresh':	'',
