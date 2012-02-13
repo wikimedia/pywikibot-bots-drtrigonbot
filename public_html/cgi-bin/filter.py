f39801b7a4f5f7c9d2681fcb9a862a75b50f42e2
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2012-02-13 13:00:32 +0000

    new feature; mass unshorten URLs within given page (this can be extended in future for other functions also)
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@264 f7c66474-1b4d-40db-9de1-ad65934439f2

---
+++ public_html/cgi-bin/filter.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""filter: Versatile/Generic (text) Page Filter Tool (CGI) for toolserver
+(description)
+
+to make it usable from server, use: 'chmod 755 filter.py', which results in
+'-rwxr-xr-x 1 drtrigon users 447 2009-04-16 19:13 test.py'
+"""
+
+## @package filter.py
+#  @brief   filter: Versatile/Generic (text) Page Filter Tool (CGI) for toolserver
+#
+#  @copyright Dr. Trigon, 2012
+#
+#  @section FRAMEWORK
+#
+#  Python wikipedia robot framework, DrTrigonBot.
+#  @see http://pywikipediabot.sourceforge.net/
+#  @see http://de.wikipedia.org/wiki/Benutzer:DrTrigonBot
+#
+#  @section LICENSE
+#
+#  Distributed under the terms of the MIT license.
+#  @see http://de.wikipedia.org/wiki/MIT-Lizenz
+#
+__version__='$Id$'
+#
+
+
+# 'python test_rss2html-urlunshortened.py > test.html'
+# (buffer to speed up is implemented but is still very slow,
+#  multi-threading should be implemented for unshorten calls...)
+# (security for param 'url' like in xsalt or subster HAS TO BE
+#  IMPLEMENTED - may be do this in a extra module since it is
+#  needed in various places now...)
+# (other options additional to 'unshort' can an may be should
+#  be implemented...)
+# PROBLEME mit 'Content-Type ... utf-8' ... !!!!
+
+
+# === python CGI script === === ===
+#
+import cgitb    # debug
+cgitb.enable()  #
+
+import cgi
+
+
+# === module imports === === ===
+#
+
+
+# === panel HTML stylesheets === === ===
+# MAY BE USING Cheetah (http://www.cheetahtemplate.org/) WOULD BE BETTER (or needed at any point...)
+#
+import ps_plain as style   # panel-stylesheet 'plain'
+
+
+# === page HTML contents === === ===
+#
+default_content = \
+"""<?xml version="1.0" encoding="UTF-8" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>filter: Versatile/Generic (text) Page Filter Tool</title>
+</head>
+<body>
+<h3>filter: Versatile/Generic (text) Page Filter Tool</h3>
+<p>%(footer)s</p>
+<form action="filter.py" name="">
+  <table>
+  <tr>
+    <td>url:</td>
+    <td><input name="url" type="text" size="60" maxlength="200" value="%(url)s"></td>
+    <td>(e.g. "http://blog.wikimedia.de/feed/")</td>
+  </tr>
+  <tr>
+    <td>unshorten urls:</td>
+    <td><input type="checkbox" name="unshort" value="1"></td>
+    <td>(this is VERY slow - be patient)</td>
+  </tr>
+  </table>
+
+  <input type="submit" value=" OK ">
+</form>
+</body>
+</html>"""
+
+
+# === CGI/HTML page view user interfaces === === ===
+#
+
+
+# === Main procedure entry point === === ===
+#
+form = cgi.FieldStorage()
+
+# operational mode
+param = {    'url': form.getvalue('url', ''),
+         'unshort': form.getvalue('unshort', ''),
+        }
+url   = param['url']
+
+# security
+# http://lists.wikimedia.org/pipermail/toolserver-l/2011-September/004403.html
+# check url not to point to a local file on the server, e.g. 'file://'
+# (same code as used in subster.py)
+s1 = False
+for item in ['http://', 'https://']:
+    s1 = s1 or (url[:len(item)] == item)
+secure = s1
+
+content_type = style.content_type
+
+
+if secure and url:
+    import urllib, re, BeautifulSoup, time
+
+    # external unshortener API from ...
+    #unshort   = "http://realurl.org/api/v1/getrealurl.php?url=%s"
+    unshort   = "http://www.unshorten.it/api1.0.php?shortURL=%s&responseFormat=xml"
+    skip_list = ['://identi.ca',
+                 '.wikimedia.org',
+                 '://wikimediafoundation.org',
+                ]
+
+    info = []
+
+    tot_tic = time.time()
+
+    # http://docs.python.org/library/urllib.html
+    # http://docs.python.org/library/mimetools.html#mimetools.Message
+    #url = urllib.unquote(url)
+    f = urllib.urlopen(url)
+    page_buf  = f.read()
+    page_info = f.info()
+    #print page_buf
+
+    # prevent '502 bad gateway' errors
+#    content_type = '; '.join(['Content-Type: %s' % page_info.gettype()] + page_info.getplist())
+    content_type = "Content-Type: text/html;\n"
+
+    toc = time.time()
+    info += [ 'Page served by %s in %.3f secs.' % (url.split('/')[2], toc-tot_tic) ]
+
+    if param['unshort']:
+        tic = time.time()
+
+        r = re.compile(r'(http(s?)://[^ "<]+)')
+        url_list = r.findall(page_buf)
+        for (url, other) in set(url_list):
+            skip = False
+            for skip_url in skip_list:
+                 if skip_url in url:
+                     skip = True
+                     break
+            if skip: continue
+
+            unshort_buf = urllib.urlopen(unshort % url).read()
+            bs          = BeautifulSoup.BeautifulSoup(unshort_buf)
+            #if (bs.status.contents[0] == "1"):
+            #    longurl = str(bs.real.contents[0])
+            #    page_buf = page_buf.replace(url, longurl)
+            if not bs.findAll('error'):
+                longurl = str(bs.fullurl.contents[0])
+                page_buf = page_buf.replace(url, longurl)
+
+        toc = time.time()
+        info += [ 'UnShortened by %s in %.3f secs.' % (unshort.split('/')[2], toc-tic) ]
+
+    tot_toc = time.time()
+    info = [ 'Served by toolserver.org in %.3f secs.' % (tot_toc-tot_tic) ] + info
+
+    #page_buf += '\n<!-- %s -->' % '\n'.join(info)
+    page_buf += '\n<!-- \n%s\n -->' % '\n'.join(info)
+
+else:
+    param.update({'footer': style.footer + style.footer_w3c})
+    page_buf = default_content % param
+
+
+# send page to browser/receiver
+print content_type
+print page_buf
