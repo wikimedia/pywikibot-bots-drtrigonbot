be942f32e8ffa77ec27867ddffb12da7ccce4d6c
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2012-05-06 13:01:18 +0000

    follow-up; JSEG added, permission in 'pywikipedia/jseg/JSEG_Commons_bot.txt'
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@310 f7c66474-1b4d-40db-9de1-ad65934439f2

---
+++ pywikipedia/jseg/__init__.py
@@ -0,0 +1,17 @@
+try:
+    # try to import
+    import segdist_cpp
+except ImportError:
+    import os
+
+    cur = os.path.abspath(os.curdir)
+    os.chdir( os.path.join(os.path.abspath(os.curdir), 'jseg') )
+
+    # compile python module (may be use 'distutil' instead of 'make' here)
+    if os.system("make segdist_cpp.so"):
+        raise ImportError("'segdist_cpp.so' could not be compiled!")
+
+    os.chdir( cur )
+
+    # re-try to import
+    import segdist_cpp
---
+++ pywikipedia/jseg/main.hpp
@@ -0,0 +1,9 @@
+#ifndef __MAIN_HPP
+#define __MAIN_HPP
+
+// to be able to use it in C++ without changing the code from C to C++
+extern "C" {
+  void worker (int argc, char *argv[]);
+} 
+
+#endif
