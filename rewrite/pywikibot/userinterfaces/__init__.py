28768b1dfc5a3387657ee74cc43d0b371ad15881
drtrigon <drtrigon@f7c66474-1b4d-40db-9de1-ad65934439f2>
2013-04-25 12:50:14 +0000

    improvement; the whole rewrite branch is upstream now (pywikipedia framework)
    git-svn-id: http://svn.toolserver.org/svnroot/drtrigon@529 f7c66474-1b4d-40db-9de1-ad65934439f2

--- rewrite/pywikibot/userinterfaces/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8  -*-
-#
-# (C) Pywikipedia bot team, 2007
-#
-# Distributed under the terms of the MIT license.
-#
-__version__ = '$Id: __init__.py 6478 2009-03-03 10:23:26Z nicdumz $'
